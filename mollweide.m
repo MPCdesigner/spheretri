8221dc881b54271107ffd6cf75eaae6235ab2ee4
Nike Dattani <nike@hpqc.org>
2022-05-31 16:38:32 -0400

    The following things:
    - Henry's copy+paste from online MATLAB didn't get the last two lines, so I put them in
    - Removed the outputting of "a" which was only there for debugging purposes.
    - Added comments to explain what we did in in the Zoom meeting
    - Made aesthetic improvements.

--- mollweide.m
+++ mollweide.m
@@ -1,18 +1,20 @@
-function [X, Y, a] =  mollweide(vMat,R)               % vMat = [x y z] where x,y,z are vectors. R is the radius
+function [X, Y] =  mollweide(v,R)                                 % v = [x y z] where x,y,z are vectors. R is the radius
 
-x = vMat(:,1);
-y = vMat(:,2);
-z = vMat(:,3);
+x = v(:,1); y = v(:,2); z = v(:,3);
 
-phi   = atan2(y,x);                                % Azimuth
-theta = acos(z./sqrt(x.^2 + y.^2 + z.^2));         % Polar angle
-eps = 1e-6;
-latitude = pi/2 - theta;
+phi   = atan2(y,x);                                               % Azimuth
+theta = acos(z./sqrt(x.^2 + y.^2 + z.^2));                        % Polar angle
+latitude = pi/2 - theta;                                          % phi from Wikipedia = pi/2 - theta = latitude. 
+
+tol = 1e-6;
+
 for k=1:length(theta)
-    if (abs(latitude(k)) > pi/2 - eps)
-        a(k,1) = latitude(k);
+    f = @(a) 2*a + sin(2*a) - pi*sin(latitude(k));                % f = 2a + sin(2a) = pi*sin(pi/2 - theta) is the function we want to find the root of. theta is a constant and "a" is what we're trying to find.
+    if (abs(latitude(k)-pi/2) < tol); a(k,1) = latitude(k);       % If |latitude(k) - pi/2| is close to 0, then it means we're at the North or South pole. Wikipedia says: (+/-)phi = pi/2  => (+/-)theta=pi/2 => skip the root-finding algorithm.
     else
-        f = @(a) 2*a + sin(2*a) - pi*sin(latitude(k)); % phi from Wikipedia = pi/2 - theta. Find the angle "a" such that 2a + sin(2a) = pi*sin(pi/2 - theta)
-        a(k,1) = fzero(f,latitude(k));                      % find the angle "a" where the function "f" is 0. Use "a = phi(k)" as the intial guess.
+        a(k,1) = fzero(f,latitude(k));                            % find the angle "a" where the function "f" is 0. Use "a = latitude(k)" as the intial guess.
     end
 end
+
+Y = R*sqrt(2)*sin(a);                                             % a = the theta in https://en.wikipedia.org/w/index.php?title=Mollweide_projection&oldid=1073635466
+X = R*2*sqrt(2)*cos(a).*(phi-pi)/pi;                              % (lambda - lamdba0)/pi = (phi-pi)/pi). lamdda0=0; lambda=phi-pi.
