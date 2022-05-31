9ae4867724fb3e917c46dd993340c4df73816272
unknown <henryliang416525@gmail.com>
2022-05-31 13:02:25 -0700

    modified mollweide.m after meeting

--- mollweide.m
+++ mollweide.m
@@ -1,16 +1,18 @@
-function [X, Y] =  mollweide(vMat,R)               % vMat = [x y z] where x,y,z are vectors. R is the radius
+function [X, Y, a] =  mollweide(vMat,R)               % vMat = [x y z] where x,y,z are vectors. R is the radius
 
 x = vMat(:,1);
 y = vMat(:,2);
 z = vMat(:,3);
 
-phi   = atan(y./x);                                % Azimuth
+phi   = atan2(y,x);                                % Azimuth
 theta = acos(z./sqrt(x.^2 + y.^2 + z.^2));         % Polar angle
-
-for k=1:length(phi)
-    f = @(a) 2*a + sin(2*a) - pi*sin(pi/2-phi(k)); % phi from Wikipedia = pi/2 - theta. Find the angle "a" such that 2a + sin(2a) = pi*sin(pi/2 - theta)
-    a(k,1) = fzero(f,phi(k));                      % find the angle "a" where the function "f" is 0. Use "a = phi(k)" as the intial guess.
+eps = 1e-6;
+latitude = pi/2 - theta;
+for k=1:length(theta)
+    if (abs(latitude(k)) > pi/2 - eps)
+        a(k,1) = latitude(k);
+    else
+        f = @(a) 2*a + sin(2*a) - pi*sin(latitude(k)); % phi from Wikipedia = pi/2 - theta. Find the angle "a" such that 2a + sin(2a) = pi*sin(pi/2 - theta)
+        a(k,1) = fzero(f,latitude(k));                      % find the angle "a" where the function "f" is 0. Use "a = phi(k)" as the intial guess.
+    end
 end
-
-Y = R*sqrt(2)*sin(a);                              % a = the theta in https://en.wikipedia.org/w/index.php?title=Mollweide_projection&oldid=1073635466
-X = R*2*sqrt(2)*cos(a).*(1/2-theta/pi);            % lambda = pi/2 - theta, lambda0 = 0. So (lambda - lamdba0)/pi = (1/2 - theta/pi);
