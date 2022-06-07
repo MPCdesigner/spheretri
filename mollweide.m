762ac2a1b1de60ab4add823c7e405b6b43556baa
MPCdesigner <63942604+MPCdesigner@users.noreply.github.com>
2022-06-07 14:03:12 -0700

    Update mollweide.m

--- mollweide.m
+++ mollweide.m
@@ -1,13 +1,9 @@
-function [X, Y] =  mollweide(v,R)                                 % v = [x y z] where x,y,z are vectors. R is the radius
-
+function [X, Y, indicesOfBadPairs] =  mollweide(v,R)
 x = v(:,1); y = v(:,2); z = v(:,3);
-
 phi   = atan2(y,x);                                               % Azimuth
 theta = acos(z./sqrt(x.^2 + y.^2 + z.^2));                        % Polar angle
-latitude = pi/2 - theta;                                          % phi from Wikipedia = pi/2 - theta = latitude. 
-
+latitude = pi/2 - theta;                                          % phi from Wikipedia = pi/2 - theta = latitude.
 tol = 1e-6;
-
 for k=1:length(theta)
     f = @(a) 2*a + sin(2*a) - pi*sin(latitude(k));                % f = 2a + sin(2a) = pi*sin(pi/2 - theta) is the function we want to find the root of. theta is a constant and "a" is what we're trying to find.
     if (abs(latitude(k)-pi/2) < tol); a(k,1) = latitude(k);       % If |latitude(k) - pi/2| is close to 0, then it means we're at the North or South pole. Wikipedia says: (+/-)phi = pi/2  => (+/-)theta=pi/2 => skip the root-finding algorithm.
@@ -15,6 +11,7 @@ for k=1:length(theta)
         a(k,1) = fzero(f,latitude(k));                            % find the angle "a" where the function "f" is 0. Use "a = latitude(k)" as the intial guess.
     end
 end
-
 Y = R*sqrt(2)*sin(a);                                             % a = the theta in https://en.wikipedia.org/w/index.php?title=Mollweide_projection&oldid=1073635466
 X = R*2*sqrt(2)*cos(a).*phi/pi;                                   % (lambda - lamdba0)/pi = phi/pi. lamdda0=0; lambda=phi.
+
+indicesOfBadPairs=abs(phi.'-phi)>pi;
